aa538e4136659421efb07f3a264e8b84579c9f10
Mansha <manshapreet02@gmail.com>
2024-04-03 20:01:36 +0530

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import footerbg from '../assets/footer/footerbg.png'
+import footerbgm from '../assets/footer/footerbg_m.png'
 import ig from '../assets/footer/ig.png'
 import linkedin from '../assets/footer/linkedin.png'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 const Footer = ({contact_ref}) => {
   return (
@@ -18,6 +20,29 @@ const Footer = ({contact_ref}) => {
         </div>
 
         <div className='footerContent'>
+          <div className='footerCopyright' style={{textAlign:'left'}}>
+
+            <div className='footerContact'>Contact Us</div>
+              <div className='footerLinks'>
+                {/* <Link to='https://www.instagram.com/blissfulminds_bvcoe/' target='_blank'><img src={ig} className='footerIcons'/></Link> */}
+                <Link to='https://www.instagram.com/p/C48WDEcSKzm/' target='_blank'><img src={ig} className='footerIcons'/></Link>
+                <Link to='https://www.linkedin.com/company/blissful-minds-society/' target='_blank'><img src={linkedin} className='footerIcons'/></Link>
+            </div>
+          {/* <img src={logo} className='footerLogo'/> */}
+              <div>© 2024 Blissful Minds Society. All Rights Reserved</div>
+          </div>
+          
+
+          <div className='footerCredits'>
+              <div>Developed & Designed by :</div>
+              <div className='footerCreditsNames'>
+                  <div>Mayank</div><div>Tushar</div><div>Mansha</div>
+              </div> 
+          </div>
+          <div className='footerBMS'><img className='footerbgdesk' src={footerbg}/><img className='footerbgmobile' src={footerbg}/></div>
+        </div>
+
+        <div className='footerContent_mobile'>
           {/* <div className='footerAnchors'>
               <div>Home</div>
               <div>About</div>
@@ -31,7 +56,10 @@ const Footer = ({contact_ref}) => {
             <Link to='https://www.instagram.com/p/C48WDEcSKzm/' target='_blank'><img src={ig} className='footerIcons'/></Link>
             <Link to='https://www.linkedin.com/company/blissful-minds-society/' target='_blank'><img src={linkedin} className='footerIcons'/></Link>
           </div>
-          <div className='footerCopyright'>copyright © blissfulmindssociety</div>
+          <div className='footerCopyright'>
+            <div>© 2024 Blissful Minds Society. All Rights Reserved</div> 
+            <div>Developed & Designed by Mayank, Mansha & Tushar</div>
+            </div>
           <div className='footerBMS'><img className='' src={footerbg}/></div>
         </div>
     </div>
