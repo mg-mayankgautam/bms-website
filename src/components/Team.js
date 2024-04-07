8a010aa70f7b82618d49c6e99d075d35fcd36f67
Mansha <manshapreet02@gmail.com>
2024-04-07 20:36:26 +0530

    gradients etc

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import image1 from '../assets/Tran.png'
+// import image1 from '../assets/Tran.png'
 import { useRef, } from "react";
 import { motion, useScroll,useMotionValueEvent,useInView } from "framer-motion";
 
