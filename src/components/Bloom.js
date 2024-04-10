f09ab690e98104a05da5d3f95aa7360ce5040d97
John Doe <johndoe@email.com>
2024-04-10 19:45:57 +0530

    n

--- src/components/Bloom.js
+++ src/components/Bloom.js
@@ -1,15 +1,16 @@
-import React from 'react'
-
+import React from 'react';
+import Nav from './Nav';
+import logo from '../assets/logo.png'
 const Bloom = () => {
   return (
-    <div style={{height:'100vh'}} >
-        
+    <div style={{height:'200vh'}} >
+        <Nav/>
         <div className='Banner_bloom' >
-            <div style={{}}>logo</div>
-            <div>bloom</div>
-            <div>mental health fest</div>
+            <div ><img style={{height:'5rem'}} src={logo}/></div>
+            <div style={{alignSelf:'center', fontSize:'8rem'}}>bloom</div>
+            <div style={{alignSelf:'end', fontSize:'8rem'}}>a mental health fest</div>
         </div>
-      this is the bloom page
+      
     </div>
   )
 }
